030c558fc0db1a1dddb0cd48b8006da97f83948d
TroyKey <tkv9795@saintjoe.edu>
2015-05-06 13:40:44 -0500

    Update array.js

--- array.js
+++ array.js
@@ -1,3 +1,9 @@
 
 var part = ["knee", "hand", "head", "arm"];
 //making the array of objects
+part = new int[3];//4 parts of the arrrat
+part[0] = 10
+part[1] = 20
+part[2]=30
+part[3] = 40
+//assigning values to elements of the array
